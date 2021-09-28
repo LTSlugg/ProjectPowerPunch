6a75dd0fc060f9328938108bd27090cbb774c95e
Luis Troncoso <LTSlugg@gmail.com>
2021-09-28 15:30:17 -0600

    Planning-Prototyping Entry

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*TODO: This.Now
+ * Handles User Input 
+ * Communicates with Animator
+ * Communicates with Other Game Systems
+ * Communicates with Player Game Systems (HP, Daze Meter, Block Meter)
+ * 
+ * TODO: WASD Control Scheme two Buttons for attack***************
+ */
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private Transform[] TransformPositionArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        TrackMoveDirection();
+    }
+
+
+    private void TrackMoveDirection()
+    {
+        if(Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0) 
+            { this.transform.position = TransformPositionArray[0].position; }
+
+        switch (Input.GetAxisRaw("Vertical"))
+        {
+            case 1:
+                transform.position = TransformPositionArray[2].position;
+                break;
+
+            case -1:
+                break;
+        }
+
+        switch (Input.GetAxisRaw("Horizontal"))
+        {
+            case 1:
+                transform.position = TransformPositionArray[1].position;
+                break;
+
+            case -1:
+                transform.position = TransformPositionArray[3].position;
+                break;
+        }
+    }
+
+
+}
