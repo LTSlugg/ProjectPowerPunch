c6cbf5e6ca8dd6a380b58170549a6f2da608ff0d
Luis Troncoso <LTSlugg@gmail.com>
2021-10-03 20:48:31 -0600

    Planning-Prototyping Phase V.03
    Extrapolated P.S.M Code from PlayerController Script to allow for easier reading and functionality

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -17,13 +17,13 @@ public class PlayerController : MonoBehaviour
     //Player Data
     [SerializeField] private Transform[] transformPositionArray;
     [SerializeField] private int currentPlayerPosition;
-    [SerializeField] private float attackSpeedRate = .15f;
+    [SerializeField] public float attackSpeedRate { get; private set; }
 
 
-    //Game Object Components
+    //Game Object Components / Scripts
     Animator _animator;
     Rigidbody2D _rgbd2;
-
+    [SerializeField]PlayerStateMachine _playerStateMachine;
 
 
     //*******************************************************************************************************************
@@ -34,24 +34,17 @@ public class PlayerController : MonoBehaviour
         _animator = GetComponent<Animator>();
         _rgbd2 = GetComponent<Rigidbody2D>();
 
-        currentState = ActionStates.Idle; //Sets default state
-        previousState = currentState; //Sets the previous state to this current state
+
+        attackSpeedRate = .15f;
         currentAttackCoolDown = attackSpeedRate; //Sets the current cooldown to the attackspeed rate
     }
 
     //Logic Update is called once per frame
     void Update()
     {
-        StateHandler();
-        ResetPreviousStateAnimation();
         TrackPlayerAttackLogicFunction();
         PlayerAttackFunction();
         MoveDirection();
-    }
-
-    //Physics Update
-    void FixedUpdate()
-    {
     }
 
     #endregion
@@ -59,7 +52,7 @@ public class PlayerController : MonoBehaviour
 
 
     //*************************************************************************************************************
-    #region Player Movement
+    #region Player Movement/Input
     /*WASD Control Scheme Two Buttons for attack***************
     *   
     * --2--  |   --W--       <---Array Diagram 
@@ -76,7 +69,7 @@ public class PlayerController : MonoBehaviour
             { 
                 this.transform.position = transformPositionArray[0].position;
                 this.currentPlayerPosition = 0;
-                if(currentState == ActionStates.Blocking) { ResetStateToIdle(); } //Will reset to Idle if no input is in and the current State is Blocking to leave block state
+                if(_playerStateMachine.currentState == PlayerStateMachine.ActionStates.Blocking) { _playerStateMachine.ResetStateToIdle(); } //Will reset to Idle if no input is in and the current State is Blocking to leave block state
             }
 
         //TODO: ADD DASH ANIMATION EFFECT
@@ -103,7 +96,7 @@ public class PlayerController : MonoBehaviour
                 if (currentPlayerPosition == 1 || currentPlayerPosition == 3) { break; };
                 transform.position = transformPositionArray[2].position;
                 this.currentPlayerPosition = 2;
-                this.currentState = ActionStates.Blocking;
+                _playerStateMachine.currentState = PlayerStateMachine.ActionStates.Blocking;
                 break;
 
                 //TODO: ADD POWERPUNCH LOGIC
@@ -123,17 +116,17 @@ public class PlayerController : MonoBehaviour
         switch (canAttack)
         {
             case true:
-                if (Input.GetKeyDown(KeyCode.Z) && currentState != ActionStates.Blocking && !isDoingReset)
+                if (Input.GetKeyDown(KeyCode.Z) && _playerStateMachine.currentState != PlayerStateMachine.ActionStates.Blocking && !_playerStateMachine.isDoingReset)
                 {
-                    currentState = ActionStates.AttackJab;
+                    _playerStateMachine.currentState = PlayerStateMachine.ActionStates.AttackJab;
                     currentAttackCoolDown = 0f;
-                    isResetCalled = true;
+                    _playerStateMachine.isResetCalled = true;
                 }
-                else if (Input.GetKeyDown(KeyCode.C) && currentState != ActionStates.Blocking && !isResetCalled)
+                else if (Input.GetKeyDown(KeyCode.C) && _playerStateMachine.currentState != PlayerStateMachine.ActionStates.Blocking && !_playerStateMachine.isDoingReset)
                 {
-                    currentState = ActionStates.AttackCross;
+                    _playerStateMachine.currentState = PlayerStateMachine.ActionStates.AttackCross;
                     currentAttackCoolDown = 0f;
-                    isResetCalled = true;
+                    _playerStateMachine.isResetCalled = true;
                 }
                 break;
 
@@ -161,144 +154,8 @@ public class PlayerController : MonoBehaviour
 
 
     //**************************************************************************************************************************
-    #region The Players State Machine
 
-
-    private enum ActionStates
-    {
-        Idle,
-        Blocking,
-        AttackJab,
-        AttackCross,
-        GotHit,
-        Moving,
-        Hurt,
-        Dazed,
-        KnockedOut,
-    }
-    [SerializeField] private ActionStates currentState;
-    private ActionStates previousState;
-    //Handles telling Animator when to play certain animations depending on the current state the player is in.
-    private void StateHandler()
-    {
-        switch (this.currentState)
-        {
-            case ActionStates.Idle:
-                _animator.SetTrigger("Idle");
-                break;
-
-            case ActionStates.Blocking:
-                _animator.SetTrigger("Block");
-                break;
-
-            case ActionStates.AttackJab:
-                _animator.SetTrigger("Jab");
-                CallResetToIdle();
-                break;
-
-            case ActionStates.AttackCross:
-                _animator.SetTrigger("Cross");
-                CallResetToIdle();
-                break;
-            
-            case ActionStates.GotHit:
-                break;
-            
-            case ActionStates.Moving:
-                break;
-            
-            case ActionStates.Hurt:
-                break;
-            
-            case ActionStates.Dazed:
-                break;
-            
-            case ActionStates.KnockedOut:
-                break;
-        }
-    }
-
-
-    //Resets the current State
-    private void ResetStateToIdle()
-    {
-        _animator.ResetTrigger(FindStateTriggerName(currentState));
-        currentState = ActionStates.Idle;
-    }
-
-
-    //Timed Reset of the current State to Default
-    private bool isResetCalled = false; //Tracks when the Reset function is called
-    private bool isDoingReset = false; //Tracks if the Coroutine is doing the reset
-    private IEnumerator TimedResetToIdle()
-    {
-        while (isDoingReset)
-        {
-            yield return new WaitForSeconds(attackSpeedRate); //wait the attack speed rate before resetting to idle animation
-            ResetStateToIdle();
-            isDoingReset = false; //Turns off its own infinite loop with this boolean
-        }
-    }
-    //A Logic Function that calls the Coroutine to reset the current state to Idle after a set time
-    private void CallResetToIdle()
-    {
-        if (isResetCalled && !isDoingReset)
-        {
-            isDoingReset = true;
-            StartCoroutine("TimedResetToIdle");
-            isResetCalled = false;
-        }
-    }
-
-
-
-    //Handles reseting the current trigger state the player is in
-    private void ResetPreviousStateAnimation()
-    {
-        if(currentState != previousState) 
-        {
-            _animator.ResetTrigger(FindStateTriggerName(previousState));
-            previousState = currentState;
-        }
-    }
-
-
-    //Returns the current state trigger name that is parsed through
-    private string FindStateTriggerName(ActionStates StateToTrack)
-    {
-        switch (StateToTrack)
-        {
-            case ActionStates.Idle:
-                return "Idle";
-
-            case ActionStates.Blocking:
-                return "Block";
-
-            case ActionStates.AttackJab:
-                return "Jab";
-
-            case ActionStates.AttackCross:
-                return "Cross";
-
-            case ActionStates.GotHit:
-                break;
-
-            case ActionStates.Moving:
-                break;
-
-            case ActionStates.Hurt:
-                break;
-
-            case ActionStates.Dazed:
-                break;
-
-            case ActionStates.KnockedOut:
-                break;
-        }
-
-        Debug.LogError("Returning the current State is Broken");
-        return null;
-    }
-    #endregion
 
 }
+
+
---
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    #region The Players State Machine
+    //Communicates with the ANimator and allows the proper Animation and Logic to run corresponding with the current Players State
+    //This depends entirely upon the Input from the Player Controller
+
+
+    private Animator _animator;
+
+    public enum ActionStates
+    {
+        Idle,
+        Blocking,
+        AttackJab,
+        AttackCross,
+        GotHit,
+        Moving,
+        Hurt,
+        Dazed,
+        KnockedOut,
+    }
+    public ActionStates currentState;
+    public ActionStates previousState;
+
+    public void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+        currentState = PlayerStateMachine.ActionStates.Idle; //Sets default state
+        previousState = currentState; //Sets the previous state to this current state
+    }
+
+    void Update()
+    {
+        StateHandler();
+        ResetPreviousStateAnimation();
+    }
+
+    //Handles telling Animator when to play certain animations depending on the current state the player is in.
+    public void StateHandler()
+    {
+        switch (this.currentState)
+        {
+            case ActionStates.Idle:
+                _animator.SetTrigger("Idle");
+                break;
+
+            case ActionStates.Blocking:
+                _animator.SetTrigger("Block");
+                break;
+
+            case ActionStates.AttackJab:
+                _animator.SetTrigger("Jab");
+                CallResetToIdleRoutine();
+                break;
+
+            case ActionStates.AttackCross:
+                _animator.SetTrigger("Cross");
+                CallResetToIdleRoutine();
+                break;
+
+            case ActionStates.GotHit:
+                break;
+
+            case ActionStates.Moving:
+                break;
+
+            case ActionStates.Hurt:
+                break;
+
+            case ActionStates.Dazed:
+                break;
+
+            case ActionStates.KnockedOut:
+                break;
+        }
+    }
+
+
+    //Resets the current State
+    public void ResetStateToIdle()
+    {
+        _animator.ResetTrigger(FindStateTriggerName(currentState));
+        currentState = ActionStates.Idle;
+    }
+
+
+    //Timed Reset of the current State to Default
+    public bool isResetCalled = false; //Tracks when the Reset function is called
+    public bool isDoingReset = false; //Tracks if the Coroutine is doing the reset
+    private IEnumerator TimedResetToIdle()
+    {
+        while (isDoingReset)
+        {
+            yield return new WaitForSeconds(GetComponent<PlayerController>().attackSpeedRate); //wait the attack speed rate before resetting to idle animation
+            ResetStateToIdle();
+            isDoingReset = false; //Turns off its own infinite loop with this boolean
+        }
+    }
+    //A Logic Function that calls the Coroutine to reset the current state to Idle after a set time
+    private void CallResetToIdleRoutine()
+    {
+        if (isResetCalled && !isDoingReset)
+        {
+            isDoingReset = true;
+            StartCoroutine("TimedResetToIdle");
+            isResetCalled = false;
+        }
+    }
+
+
+
+    //Handles reseting the current trigger state the player is in
+    public void ResetPreviousStateAnimation()
+    {
+        if (currentState != previousState)
+        {
+            _animator.ResetTrigger(FindStateTriggerName(previousState));
+            previousState = currentState;
+        }
+    }
+
+
+    //Returns the current state trigger name that is parsed through
+    private string FindStateTriggerName(ActionStates StateToTrack)
+    {
+        switch (StateToTrack)
+        {
+            case ActionStates.Idle:
+                return "Idle";
+
+            case ActionStates.Blocking:
+                return "Block";
+
+            case ActionStates.AttackJab:
+                return "Jab";
+
+            case ActionStates.AttackCross:
+                return "Cross";
+
+            case ActionStates.GotHit:
+                break;
+
+            case ActionStates.Moving:
+                break;
+
+            case ActionStates.Hurt:
+                break;
+
+            case ActionStates.Dazed:
+                break;
+
+            case ActionStates.KnockedOut:
+                break;
+        }
+
+        Debug.LogError("Returning the current State is Broken");
+        return null;
+    }
+    #endregion
+}
